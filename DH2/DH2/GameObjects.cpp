1f513079bdd0386643c342be42e019323c3858af
Unknown <obliviondrums@yahoo.com>
2018-05-14 08:49:45 -0700

    Made getters and setters for GameObject pos and size. Removed texture from the base object as not all objects will need one.

--- DH2/DH2/GameObjects.cpp
+++ DH2/DH2/GameObjects.cpp
@@ -1,12 +1,7 @@
-
 #include "GameObjects.h"
-#include <iostream>
 
-
-GameObject::GameObject(std::string texturePath, sf::Vector2f pos, sf::Vector2f size) 
-	:m_pos(pos), m_size(size)
+GameObject::GameObject(sf::Vector2f pos, sf::Vector2f size) : m_pos(pos), m_size(size)
 {
-	
 }
 
 void GameObject::Draw(sf::RenderWindow * window)
