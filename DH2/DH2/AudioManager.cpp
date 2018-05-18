bfd5d255cb9cf8c33047c886d7f9138e697c4e93
Christopher Lozano <UchihaKite@gmail.com>
2018-05-18 11:19:17 -0700

    Completed AudioManager
    In order to correctly format the Text File that lists all the Audio Files being Imported into the Program, and the Enum Class, please read ALL Comments in AudioManager.h and AudioManager.cpp

--- DH2/DH2/AudioManager.cpp
+++ DH2/DH2/AudioManager.cpp
@@ -1,6 +1,10 @@
 #include "AudioManager.h"
+#include <fstream>
+#include <iostream>
 
 AudioManager::AudioManager()
+	: m_TextFileName("Name.txt") // Change "Name.txt" to the correct file name that has the names of the Audio File
+	                             // Make sure, in the Text File, that each line does not end with a "Space (White Space)"
 {
 	GenerateFileNames();
 	GenerateBuffers();
@@ -14,12 +18,51 @@ sf::Sound AudioManager::PlaySound(ListOfAudio Audio)
 
 void AudioManager::GenerateFileNames()
 {
+	std::ifstream s_InFile;
+	s_InFile.open(m_TextFileName);
+
+	if (s_InFile.is_open())
+	{
+		int s_Tick = 0;
+		while (!s_InFile.eof())
+		{
+			std::getline(s_InFile, m_FileNames[s_Tick]);
+			s_InFile.get(); // Gets rid of the "/n" at the end of each line
+
+			if (!s_InFile)
+			{
+				break;
+			}
+
+			s_Tick++;
+		}
+	}
+	else
+	{
+		std::cout << "Cannot Open File: " << m_TextFileName << std::endl;
+	}
 }
 
 void AudioManager::GenerateBuffers()
 {
+	ListOfAudio s_Temp = ListOfAudio::FIRST;
+
+	for (int i = 0; i < m_FileNames.size(); i++)
+	{
+		if (!m_TempBuffer.loadFromFile(m_FileNames[i]))
+		{
+			m_SoundBuffers[s_Temp] = m_TempBuffer;
+			static_cast<ListOfAudio>(s_Temp + 1);
+		}
+	}
 }
 
 void AudioManager::GenerateSounds()
 {
+
+	for (ListOfAudio i = ListOfAudio::FIRST; i < ListOfAudio::COUNT; static_cast<ListOfAudio>(i + 1))
+	{
+		m_TempSound.setBuffer(m_SoundBuffers[i]);
+		m_Sounds[i] = m_TempSound;
+	}
 }
--- DH2/DH2/AudioManager.h
+++ DH2/DH2/AudioManager.h
@@ -2,8 +2,12 @@
 #include <SFML/Audio.hpp>
 #include <map>
 
+// Make sure that whichever order we have the filenames in,
+// in the text file, is the same order we have them in
+// "ListOfAudio"
 enum ListOfAudio
 {
+	FIRST, // Change this to whatever the first "Audio" is
 	COUNT // List Audio Files Here to use for the Map Below
 };
 
@@ -22,6 +26,7 @@ private: // Variables
 	std::map<ListOfAudio, sf::SoundBuffer> m_SoundBuffers;
 	std::map<ListOfAudio, sf::Sound> m_Sounds;
 	std::vector<std::string> m_FileNames;
+	std::string m_TextFileName;
 	sf::SoundBuffer m_TempBuffer;
 	sf::Sound m_TempSound;
 };
