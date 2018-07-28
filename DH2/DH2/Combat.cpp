2b3fb770466a376551f1347e2144f7eda4877e99
Unknown <obliviondrums@yahoo.com>
2018-07-28 11:47:25 -0700

    Combat class
    Added some combat behavior, some getters for characters, etc. Pretty small push

---
+++ DH2/DH2/Combat.cpp
@@ -0,0 +1,85 @@
+#include "Combat.h"
+
+
+
+Combat::Combat(std::vector<ICharacter*> characterList) : m_playerGroup(characterList)
+{
+}
+
+
+Combat::~Combat()
+{
+}
+
+void Combat::StartCombat()
+{
+	//Loop through players and enemies to determine turn order
+	//Consider if we want it to be speed, or if we do all player characters, then all enemies
+}
+
+void Combat::EndCombat()
+{
+	if (m_playerGroup.size() == 0)
+	{
+		HandlePlayerLoss();
+	}
+
+	if (m_enemyGroup.size() == 0)
+	{
+		AssignExpToPlayerCharacters();
+	}
+}
+
+void Combat::StartTurn()
+{
+}
+
+void Combat::EndTurn()
+{
+
+	//At the end of the enemy turn, clean up the target and attacker
+	CurrentTarget = nullptr; 
+	CurrentAttacker = nullptr;
+}
+
+void Combat::HandlePlayerLoss()
+{
+}
+
+void Combat::Attack(ICharacter* Attacker, ICharacter* Target)
+{
+	//Using essentially the pokemon dmg formula but took out the modifier till we figure that part out  |
+	//The formula uses BP (base power) as a multiplier as well, but I replaced (for now) with this 5 ---V
+	float Damage = (Attacker->GetLevel() + 10 / 250) * (Attacker->GetStrength() / Target->GetDefense()) * 5 + 2;
+
+	//Here we would do any kind of dodge calculation to see if the attack actually strikes.
+	bool ItDoesHit = true;
+
+	if (ItDoesHit)
+	{
+		Target->TakeDamage(Damage);
+	}
+}
+
+void Combat::Defend(ICharacter* Attacker)
+{
+	//This would mass raise the character's defense for the turn, blocking most of all incoming damage
+}
+
+void Combat::UseItem(ICharacter* Attacker, ICharacter* Target)
+{
+	//This will most likely require us to access the player's item pool, unless we're having characters have an inventory
+}
+
+void Combat::UseSkill(ICharacter* Attacker, ICharacter* Target)
+{
+}
+
+void Combat::AssignExpToPlayerCharacters()
+{
+	for (unsigned int i = 0; i < m_playerGroup.size(); i++)
+	{
+		//Need to come up with whatever exp earning formula we want for gaining levels
+		m_playerGroup[i]->GainExperience(100.f);
+	}
+}
---
+++ DH2/DH2/Combat.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+#include "ICharacter.h"
+class Combat
+{
+public:
+	Combat(std::vector<ICharacter*> characterList);
+	~Combat();
+
+	void StartCombat(); //Any setup that might need to be done during initialization.
+	void EndCombat(); //End the combat, throw up lose screen if the player lost, otherwise assign XP and do any necessary cleanup.
+
+	void StartTurn(); //Any turn setup that may be needed, set the attacker, etc.
+	void EndTurn(); //Main purpose is to clear Attacker var to be reused
+
+	void HandlePlayerLoss(); //Is this loss?
+
+	void Attack(ICharacter* Attacker, ICharacter* Target);
+	void Defend(ICharacter* Attacker);
+	void UseItem(ICharacter* Attacker, ICharacter* Target);
+	void UseSkill(ICharacter* Attacker, ICharacter* Target);
+
+	void AssignExpToPlayerCharacters();
+
+	ICharacter* CurrentAttacker;
+	ICharacter* CurrentTarget;
+
+	std::vector<ICharacter*> m_playerGroup;
+	std::vector<ICharacter*> m_enemyGroup;
+};
